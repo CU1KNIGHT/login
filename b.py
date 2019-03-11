c368f8b15d4cbc028b78f747b97e276fd5c68044
CU1KNIGHT <mah2009moud@hotmail.com>
2019-03-11 22:38:05 +0100

    hellllllllllllllllllllo

---
+++ b.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='login',
+                                       user='root',
+                                       password='')
+
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    connect()
---
+++ x.py
@@ -0,0 +1,36 @@
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+
+
+def insert_book(USERNAME, PASSWORD):
+    query = "INSERT INTO system(USERNAME,PASSWORD) " \
+            "VALUES(%s,%s)"
+    args = (USERNAME, PASSWORD)
+
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+
+        if cursor.lastrowid:
+            print('last insert id', cursor.lastrowid)
+        else:
+            print('last insert id not found')
+
+        conn.commit()
+    except Error as error:
+        print(error)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def main():
+    insert_book('mody', '1234')
+
+
+if __name__ == '__main__':
+    main()
